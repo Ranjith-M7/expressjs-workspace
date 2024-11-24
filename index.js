355040f0679de99cc22248265d6d4ef2228ec8e0
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-24 23:16:40 +0530

    updatedd

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Express.js");
+// regex
+app.get(/^\/users\/[0-9]{4}$/, (req, res) => {
+  res.send("It's working...");
 });
+
+// app.get(/x/, (req, res) => {
+//   res.send("It's working...");
+// });
+
+// app.get("/products/product/1", (req, res) => {
+//   res.send("It's working...");
+// });
 
 app.listen(8000, () => {
   console.log("Server Up!");
