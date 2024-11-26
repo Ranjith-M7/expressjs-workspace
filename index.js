f12fa16b81b729b5c5643d032493b3c3d4c50db1
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-26 23:42:15 +0530

    postman with app.route()

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ import express from "express";
 
 const app = express();
 
-// regex
-app.get(/^\/users\/[0-9]{4}$/, (req, res) => {
-  res.send("It's working...");
-});
-
-// app.get(/x/, (req, res) => {
-//   res.send("It's working...");
-// });
-
-// app.get("/products/product/1", (req, res) => {
-//   res.send("It's working...");
-// });
+//postman with app.route()
+app.route("/student")
+.get((req, res) => res.send("All student"))
+.post((req, res) => res.send("Add student"))
+.put((req, res) => res.send("Update student"))
+.delete((req, res) => res.send("Delete student"));
 
 app.listen(8000, () => {
   console.log("Server Up!");
