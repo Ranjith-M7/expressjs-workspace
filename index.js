eee9418fead072ad817ce137aadc775fb6a82a85
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-11-30 23:46:49 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from "express";
-import products from "./products.js";
+import userCredentials from "./middlewares/log.js";
+
+// validating the user request to the user, authenticate the user, authorization the usetr - middleware
+// request - middleware - response
 
 const app = express();
 
-// Sending json data from backend to client
-app.get("/product", (req, res) => {
-  res.json(products);
+app.use(userCredentials);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Screen</h1>");
+  console.log("Response ok");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About Screen</h1>");
+  console.log("Response ok");
 });
 
 app.listen(8000, () => console.log("Server up!"));
+
---
+++ middlewares/log.js
@@ -0,0 +1,7 @@
+function userCredentials(req, res, next) {
+  console.log("Username: Huxn");
+  console.log("Password: alex1234");
+  next();
+}
+
+export default userCredentials;
