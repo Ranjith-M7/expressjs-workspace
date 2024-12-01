dfa91b2890716749987a48c1e8cd30b4c7c17cde
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-12-01 23:39:23 +0530

    added serving static files using built in middleware

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express from "express";
-import userCredentials from "./middlewares/log.js";
+import path from "path";
 
-// validating the user request to the user, authenticate the user, authorization the usetr - middleware
-// request - middleware - response
+// built in middleware function serves static files
 
 const app = express();
 
-app.use(userCredentials);
+app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home Screen</h1>");
-  console.log("Response ok");
-});
-app.get("/about", (req, res) => {
-  res.send("<h1>About Screen</h1>");
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "/public/index.html"));
   console.log("Response ok");
 });
 
 app.listen(8000, () => console.log("Server up!"));
-
