f483a31d7ffd5cd56f2554f06d668835da9efe2d
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-12-02 23:41:49 +0530

    updated all express  notes and code I practiced

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import routes from "./routes/route.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use("/",routes)
+app.set("view engine", "ejs"); // tell express to use EJS
+app.use(bodyParser.json());
+app.use("/", routes);
+
+app.post("/data", (req, res) => {
+  const { name, youtube, age } = req.body;
+  res.send(`${name}, ${youtube}, ${age}`);
+});
 
 app.listen(8000, () => console.log("Server up!"));
